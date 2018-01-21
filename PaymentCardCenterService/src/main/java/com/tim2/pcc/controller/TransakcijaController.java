45a0f0ed93a2de2e875608fcaf2e6e078dc80a1c
Mirko Mikac <mmikac27@gmail.com>
2018-01-21 09:12:43 +0100

    Initial commit

---
+++ PaymentCardCenterService/src/main/java/com/tim2/pcc/controller/TransakcijaController.java
@@ -0,0 +1,10 @@
+package com.tim2.pcc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transakcija")
+public class TransakcijaController {
+	
+}
---
+++ PaymentCardCenterService/src/main/java/com/tim2/pcc/model/Banka.java
@@ -0,0 +1,71 @@
+package com.tim2.pcc.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Banka implements Serializable{
+	
+	private static final long serialVersionUID = -7475447117808950140L;
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String ime;
+	
+	@Column
+	private String adresa;
+	
+	@Column
+	private String telefon;
+
+	@Column
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getAdresa() {
+		return adresa;
+	}
+
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+
+	public String getTelefon() {
+		return telefon;
+	}
+
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
