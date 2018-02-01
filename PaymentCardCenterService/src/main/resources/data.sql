a77d540eb98da75fe05959ce2f6a892de8b4ef12
nsajic <nikola9n@gmail.com>
2018-02-01 09:23:46 +0100

    Implemented https on code-behind.

--- PaymentCardCenterService/src/main/resources/data.sql
+++ PaymentCardCenterService/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into Banka(id, ime, adresa, telefon, email, swift_code, url) values (1, "Rajfajzen", "Pere Perica 1", "021231231", "pera@pera.com", "111", "http://localhost:8300");
-insert into Banka(id, ime, adresa, telefon, email, swift_code, url) values (2, "Intesa", "Sime Simica 1", "012231231", "sima@sima.com", "789", "http://localhost:8300");
-insert into Banka(id, ime, adresa, telefon, email, swift_code, url) values (3, "Erste", "Zike Zikica 1", "023131231", "zika@zika.com", "123", "http://localhost:8300");
+insert into Banka(id, ime, adresa, telefon, email, swift_code, url) values (1, "Rajfajzen", "Pere Perica 1", "021231231", "pera@pera.com", "111", "https://localhost:8300");
+insert into Banka(id, ime, adresa, telefon, email, swift_code, url) values (2, "Intesa", "Sime Simica 1", "012231231", "sima@sima.com", "789", "https://localhost:8300");
+insert into Banka(id, ime, adresa, telefon, email, swift_code, url) values (3, "Erste", "Zike Zikica 1", "023131231", "zika@zika.com", "123", "https://localhost:8300");
